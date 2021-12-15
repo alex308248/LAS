f2e0bd03a9da708a895bbf3f536e8c9b5009916a
Alex <alex308248@gmail.com>
2021-12-14 21:24:37 -0500

    init by spring

---
+++ src/test/java/ood/proj2/las/LasApplicationTests.java
@@ -0,0 +1,13 @@
+package ood.proj2.las;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
